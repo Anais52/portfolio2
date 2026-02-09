1badbaf6295622e797194b0d058ec3397f020e15
Anais52 <annaissandrinesambou@gmail.com>
2026-02-09 23:33:13 +0000

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+
+
+with st.sidebar:
+    st.image("IMG_2839.jpeg",width=250)
+    nom=st.subheader("ANAIS SANDRINE SAMBOU")
+    st.header("🎗️TECHNICIENNE SUPERIEURE EN GEOMATIQUE")
+    email=st.write("EMAIL:annaissandrinesambou@gmail.com")
+    telephone=st.write("TELEPHONE:786620986")
+
+    st.info("• ✨Education")
+    st.write("•BTS en Géomatique")
+    st.write("•BAC L2")
+
+
+st.header (" 🫧Projets Académiques")
+st.write("• Créer des applications")
+st.write("• Ouvrir un cabinet ")          
+
+
+st.header(" 🫧Experiences Professionnelles")
+st.write("• Collecte de données a Massalikou Djinane")
+st.write("• Enquete sur le terrain")
+st.write("• Normalisation de la commune de Keur Massar")
+st.write("• Travail sur Qgis,ArcGis")
+st.write("• Travail sur python et anaconda (JupiterLaB, jupiterNotebook)")
+st.write("• Réalisation de plans 2D ET 3D")
+st.write("• Numérisation de zones géographiques")
+st.write("• Pilotage de drones")
+
+
+st.header(" 🫧Activitées")
+st.write("• Rédaction d articles ")
+st.write("• travail fréquent de groupes ")
+
+
+
+
+
+
+
+
+
